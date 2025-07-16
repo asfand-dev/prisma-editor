7c127d879f4bd9c638f9892c0a9bd714b6b2de18
Asfandiyar Khan <71268133+asfand-dev@users.noreply.github.com>
2025-07-17 00:07:26 +0200

    Removed the unused code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { createRoot } from 'react-dom/client'
+import { EnvironmentProvider } from './contexts/EnvironmentContext.tsx'
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import App from './App.tsx'
 import './index.css'
-import { EnvironmentProvider } from './contexts/EnvironmentContext.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <EnvironmentProvider>
-    <App />
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <App />
+    </TooltipProvider>
   </EnvironmentProvider>
 );
